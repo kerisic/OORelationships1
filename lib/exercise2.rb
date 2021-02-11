696b65690708612cd76b6f717d8957fc63e44354
Christine Chee <christinechee88@hotmail.com>
2021-02-11 10:54:30 +0000

    Add exercise 2 solution

---
+++ lib/exercise2.rb
@@ -0,0 +1,32 @@
+class User
+  def initialize(name, bio, age, password)
+    @name = name
+    @bio = bio
+    @age = age
+    @password = Password.new(password)
+  end
+
+  def authenticate(candidate_password)
+    @password.correct? ? true : false
+  end
+
+  def profile
+    "#{@name}, born in #{birth_year}: #{@bio}"
+  end
+
+  private
+
+  def birth_year
+    Time.new.year - @age
+  end
+end
+
+class Password
+  def initialize(password)
+    @password = password
+  end
+
+  def correct?(candidate_password)
+    @password = candidate_password
+  end
+end
